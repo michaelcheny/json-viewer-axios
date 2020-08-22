4aebf5e803a3a92fa4794654018eb531a40536f0
Michael Chen <michaelchengaming@gmail.com>
2020-08-22 15:39:04 -0700

    add reacthookforms to deal with url input

--- src/components/Fetcher.tsx
+++ src/components/Fetcher.tsx
@@ -1,17 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import axios from 'axios';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { Data } from '../interfaces/data.interface';
 
 type FetcherProps = {
   setData: React.Dispatch<React.SetStateAction<Data>>;
 };
 
-const Fetcher = ({ setData }: FetcherProps) => {
-  const [url, setUrl] = useState<string>('');
+type FormValues = {
+  url: string;
+};
 
-  const getEndpoint = async () => {
+const Fetcher = ({ setData }: FetcherProps) => {
+  const { register, handleSubmit } = useForm<FormValues>();
+
+  const onSubmit: SubmitHandler<FormValues> = async (data) => {
+    console.log(data);
+    getEndpoint(data.url);
+  };
+
+  const getEndpoint = async (path: string) => {
     try {
-      const res = await axios.get(url);
+      const res = await axios.get(path);
       console.log(res);
       setData(res);
     } catch (error) {
@@ -19,18 +29,13 @@ const Fetcher = ({ setData }: FetcherProps) => {
     }
   };
 
-  const handleSubmit = (e: React.SyntheticEvent): void => {
-    e.preventDefault();
-    getEndpoint();
-  };
-
   return (
-    <form className="input-form" onSubmit={handleSubmit}>
+    <form className="input-form" onSubmit={handleSubmit(onSubmit)}>
       <input
         type="text"
+        name="url"
         placeholder="Paste your API endpoint here..."
-        value={url}
-        onChange={(e) => setUrl(e.target.value)}
+        ref={register}
       />
     </form>
   );
